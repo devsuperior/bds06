c9794f94c051e8af060a7862b851ceed6572802f
Jeferson Willian Vieira Silva <jefersonwillians137@gmail.com>
2021-11-01 16:30:12 -0400

    Submit with react-hook-form working

--- frontweb/src/pages/Home/Login/index.tsx
+++ frontweb/src/pages/Home/Login/index.tsx
@@ -1,26 +1,39 @@
 import './styles.css';
 
+import { useForm } from 'react-hook-form';
 import Button from 'components/Button';
 
-const Login = () => {
+type FormData = {
+   username: string;
+   password: string;
+};
+
+const Login = function () {
+
+   const { register, handleSubmit } = useForm();
+   
+   const onSubmit = (formData: FormData) => {
+      console.log(formData);
+   };
+
    return (
-      <div className= "login-card">
+      <div className="login-card">
          <h1>LOGIN</h1>
-         <form>
+         <form onSubmit={handleSubmit(onSubmit)}>
             <div className="mb-4">
                <input
                   type="text"
-                  className="form-control base-input login-input"
+                  className="form-control base-input"
                   placeholder="Email"
-                  name="username"
+                  {...register('username')}
                />
             </div>
             <div className="mb-2">
                <input
                   type="password"
-                  className="form-control base-input login-input"
+                  className="form-control base-input"
                   placeholder="Senha"
-                  name="password"
+                  {...register('password')}
                />
             </div>
             <div className="login-submit">
