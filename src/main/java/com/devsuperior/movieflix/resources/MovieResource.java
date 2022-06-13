a8872d4efa326287c4b77b64a2bacc1df169adba
deophanes <deophanes@yahoo.com.br>
2022-06-13 10:13:18 -0300

    commit das atividades

--- src/main/java/com/devsuperior/movieflix/resources/MovieResource.java
+++ src/main/java/com/devsuperior/movieflix/resources/MovieResource.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.entities.Movie;
 import com.devsuperior.movieflix.entities.projection.MovieProjection;
 import com.devsuperior.movieflix.services.MovieService;
 
@@ -21,10 +23,18 @@ public class MovieResource {
 	private MovieService movieService;
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<MovieProjection> findById(@PathVariable Long id) {
-		Optional<MovieProjection> movie = movieService.findById(id);
-		if (movie.isPresent()) {
-			return new ResponseEntity<MovieProjection>(movie.get(), HttpStatus.OK);
+	public ResponseEntity<Movie> findById(@PathVariable Long id) {
+
+		Optional<MovieProjection> movieOptional = movieService.findById(id);
+		if (movieOptional.isPresent()) {
+
+			Movie movie = new Movie(Long.valueOf(movieOptional.get().getId()), movieOptional.get().getTitle(),
+					movieOptional.get().getSubTitle(), movieOptional.get().getSynopsis(),
+					Integer.valueOf(movieOptional.get().getYear()), movieOptional.get().getImgUrl(),
+					new Genre(Long.valueOf(movieOptional.get().getIdGenre()), movieOptional.get().getName(), null),
+					null);
+
+			return new ResponseEntity<Movie>(movie, HttpStatus.OK);
 		} else {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
